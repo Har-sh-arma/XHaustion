38e408906d99453d175f09e47dbdd802decbc5ca
MAYANK2264 <kmmayank08@gmail.com>
2024-06-01 20:59:28 +0530

    GPIO test

---
+++ pwm.py
@@ -0,0 +1,13 @@
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+ledpin = 12				# PWM pin connected to LED
+GPIO.setwarnings(False)			#disable warnings
+GPIO.setmode(GPIO.BOARD)		#set pin numbering system
+GPIO.setup(ledpin,GPIO.OUT)
+pi_pwm = GPIO.PWM(ledpin,1000)		#create PWM instance with frequency
+pi_pwm.start(0)				#start PWM of required Duty Cycle 
+
+while True:
+    pi_pwm.ChangeDutyCycle(50) #provide duty cycle in the range 0-100
