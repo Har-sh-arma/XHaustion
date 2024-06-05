b7706c3a1eacfe59d52c3dd34b4e6fdf942841af
Har-sh-arma <harinathsharma303@gmail.com>
2024-06-05 11:37:39 +0530

    temp sensor

--- temp_test.py/run.py
+++ temp_test.py/run.py
@@ -6,6 +6,7 @@ GPIO.setwarnings(False)			#disable warnings
 GPIO.setmode(GPIO.BOARD)		#set pin numbering system
 GPIO.setup(38,GPIO.OUT)
 GPIO.setup(40,GPIO.OUT)
+GPIO.setup(37,GPIO.IN)
 
 
 GPIO.output(38,GPIO.LOW)
@@ -13,9 +14,10 @@ GPIO.output(38,GPIO.LOW)
 n = 16
 while n:
     GPIO.output(40,GPIO.HIGH)
-    sleep(1)
+    sleep(0.5)
     GPIO.output(40,GPIO.LOW)
-    sleep(1)
+    sleep(0.5)
+    print(f"Bit {n}: {GPIO.input(37)}")
     n -= 1
 
 GPIO.output(38,GPIO.HIGH)
