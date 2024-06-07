e7ead746c1dd0ec476db9824599f737d0af11b4b
MAYANK2264 <kmmayank08@gmail.com>
2024-06-07 12:39:34 +0530

    global js conflict

--- src/global.js
+++ src/global.js
@@ -15,11 +15,111 @@ if (!flag) {
 }
 
 
+let config = {
+    "pwm_pins": [12, 32],
+    "CS_PIN": 38,
+    "SCK_PIN":40,
+    "temperature_pins": [37, 35, 33, 31, 29],
+    "exhaust_max_rpm": 5000,
+    "intake_max_rpm": 3000,
+    "has_intake": 1,
+    "num_dampers": 4,
+    "temperature_range": [
+        40,
+        70,
+        100,
+        150
+    ],
+    "passive_modes": {
+        "default": {
+            "fans": {
+                "exhaust": 10,
+                "intake": 5
+            },
+            "dampers": [
+                25,
+                25,
+                25,
+                25
+            ]
+        },
+        "1": {
+            "fans": {
+                "exhaust": 40,
+                "intake": 10
+            },
+            "dampers": [
+                35,
+                25,
+                35,
+                25
+            ]
+        },
+        "2": {
+            "fans": {
+                "exhaust": 50,
+                "intake": 10
+            },
+            "dampers": [
+                35,
+                35,
+                35,
+                35
+            ]
+        },
+        "3": {
+            "fans": {
+                "exhaust": 70,
+                "intake": 20
+            },
+            "dampers": [
+                45,
+                45,
+                45,
+                45
+            ]
+        }
+    },
+    "fan_power_scaling_for_hoods": [
+        [
+            40,
+            60,
+            80,
+            100
+        ],
+        [
+            30,
+            50,
+            70,
+            90
+        ],
+        [
+            20,
+            40,
+            50,
+            80
+        ],
+        [
+            10,
+            30,
+            50,
+            70
+        ]
+    ],
+    "damper_angles": [
+        25,
+        45,
+        60,
+        90
+    ]
+}
 
 const getSystemState = async() => {
-    let res = await fetch(`${window.location.origin}/api/system_state/` , {origin: window.location.origin})
-    let data = await res.json()
-    return JSON.parse(data)
+    // let res = await fetch(`${window.location.origin}/api/system_state/` , {origin: window.location.origin})
+    // let data = await res.json()
+    // return JSON.parse(data)
+
+
 }
 
 const getConfig = async() => {
@@ -55,6 +155,20 @@ document.addEventListener('contextmenu', event => {
 document.addEventListener("keydown", e => {
     if(e.key == "F11") e.preventDefault();
 });
-document.onkeydown = function (e) {
-    return false;
-}
+// document.onkeydown = function (e) {
+//     return false;
+// }
+document.addEventListener("gesturestart", function (e) {
+	e.preventDefault();
+    document.body.style.zoom = 0.99;
+});
+
+document.addEventListener("gesturechange", function (e) {
+	e.preventDefault();
+
+  document.body.style.zoom = 0.99;
+});
+document.addEventListener("gestureend", function (e) {
+	  e.preventDefault();
+    document.body.style.zoom = 1;
+}); 
