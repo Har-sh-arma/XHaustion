38ac0c021dc3a1e3d8884011e13b7897263f50a3
MAYANK2264 <kmmayank08@gmail.com>
2024-06-12 15:38:26 +0530

    Working version with temperature 1.0

--- src/global.js
+++ src/global.js
@@ -15,69 +15,40 @@ if (!flag) {
 }
 
 
-let conf1 = {"exhaust_pressure_range": [500, 1000, 1500, 2000], "fan_power_scaling_exhaust_pressure": [0, 25, 50, 75], "pressure_address": "0x48", "pressure_A0": "0x40", "pressure_offset": 60, "pressure_scaling": 12.5, "pwm_pins": [12, 32], "CS_PIN": 38, "SCK_PIN": 40, "temperature_pins": [37, 35, 33, 31, 29], "exhaust_max_rpm": 5000, "intake_max_rpm": 3000, "has_intake": 1, "num_dampers": 4, "damper_step": 5, "fan_step": 4, "temperature_range": [35, 71, 122, 188], "passive_modes": {"1": {"fans": {"exhaust": 100, "intake": 100}, "dampers": [18, 18, 18, 18]}, "2": {"fans": {"exhaust": 100, "intake": 50}, "dampers": [18, 18, 18, 18]}, "3": {"fans": {"exhaust": 75, "intake": 25}, "dampers": [18, 18, 18, 18]}, "default": {"fans": {"exhaust": 75, "intake": 75}, "dampers": [18, 18, 18, 18]}}, "fan_power_scaling_for_hoods": [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], "damper_angles": [25, 45, 60, 90]}
-
-let sys_st1 = {
-    "exhaust": 30,
-    "intake": 20,
-    "air_flow": 0,
-    "temperatures": [
-        0,
-        0,
-        0,
-        0
-    ],
-    "dampers": [
-        0,
-        0,
-        0,
-        0
-    ],
-    "mode": "passive",
-    "passive_mode": "default",
-    "override": {"fans":{"exhaust": 0, "intake": 0}, "dampers": [0, 0, 0, 0]}
-}
-
 const getSystemState = async() => {
-    // let res = await fetch(`${window.location.origin}/api/system_state/` , {origin: window.location.origin})
-    // let data = await res.json()
-    // return JSON.parse(data)
-    return sys_st1
+    let res = await fetch(`${window.location.origin}/api/system_state/` , {origin: window.location.origin})
+    let data = await res.json()
+    return JSON.parse(data)
 }
 
 const getConfig = async() => {
-    // let res = await fetch(`${window.location.origin}/api/config/`)
-    // let data = await res.json()
-    // return data
-    return conf1
+    let res = await fetch(`${window.location.origin}/api/config/`)
+    let data = await res.json()
+    return data
 }
 
 const setConfig = async(config) => {
-    // let res = await fetch(`${window.location.origin}/api/config/`, {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(config),
-    // })
+    let res = await fetch(`${window.location.origin}/api/config/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(config),
+    })
 
-    // remove the below in prod
-    conf1 = config
-    return
 }
 
 
 const setSystemState = async(sys_state) => {
-    // let res = await fetch(`${window.location.origin}/api/system_state/`, {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(sys_state),
-    // })
-    sys_st1 = sys_state
-    return
+    let res = await fetch(`${window.location.origin}/api/system_state/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(sys_state),
+    })
 }
+
 document.addEventListener('contextmenu', event => {
     event.preventDefault();
 });
